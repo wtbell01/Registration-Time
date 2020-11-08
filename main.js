1825fac5d4d884add10bf7d5c4aba42c2ce46ae4
William <wtbell4080@gmail.com>
2020-11-08 01:13:24 -0500

    Updated comments

--- main.js
+++ main.js
@@ -9,38 +9,34 @@ function checkInput(e){
   let classificationButtons = document.getElementsByName('classification-button');
   let errorMessage = document.getElementById('error-message');
   
-
   //Loop through classification buttons
   for(let i = 0; i < classificationButtons.length; i++){
-    /*if a button is checked and the first character of the last name is a string, call displayResult function and don't
-    display an error message*/
+    /*if a button is checked and the first character of the last name is a string,
+    call displayResult function and don't display an error message*/
     if(classificationButtons[i].checked && typeof firstLetter === 'string'){
       errorMessage.textContent = "";
       displayResult(firstLetter);
       break;
     }
-    //If a button isn't checked or the first character of the last name isn't a string, show error message
+    /*If a button isn't checked or the first character of the last name isn't a string,
+    display error message*/
     else{
       errorMessage.textContent = "*Enter last name and select classification*"
     }
-  }
-  
+  } 
   e.preventDefault();
 }
 
 
-
 function displayResult(firstLetter){
-  //Take the first letter from the last name and make it uppercase
-  
-
+  //Declare variables for buttons and registration output field
   let seniorButton= document.getElementById('senior-button');
   let juniorButton= document.getElementById('junior-button');
   let sophomoreButton= document.getElementById('sophomore-button');
   let freshmanButton= document.getElementById('freshman-button');
   let outputTimeDate = document.getElementById('output-time-date-output');
   
-
+  /*Display registration dates and times depending on the button selected and first letter of the last name*/
   if(seniorButton.checked && firstLetter <="M"){
     outputTimeDate.textContent = 'December 1st 9:00am';
   }else if(seniorButton.checked && firstLetter >="N"){
@@ -58,8 +54,6 @@ function displayResult(firstLetter){
   }else if(freshmanButton.checked && firstLetter >="N"){
     outputTimeDate.textContent = 'December 4th 11:00am';
   }
-
-  console.log(outputTimeDate.textContent);
 
 }
   
